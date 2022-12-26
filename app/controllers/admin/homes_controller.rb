0dbbc3b5b00b33586eaefc56c79889137fc75f33
yooshiinoo <112680984+yooshiinoo@users.noreply.github.com>
2022-12-26 12:39:36 +0000

    11 admin/top kaminari

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,11 @@
 class Admin::HomesController < ApplicationController
   def top
+    @posts = Post.page(params[:page])
   end
+  
+  private
+  def post_params
+    params.require(:customer).permit(:user_id, :text)
+  end
+  
 end
