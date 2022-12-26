e5098e1a57df5e8d4a1463968aa67f082768308c
yooshiinoo <112680984+yooshiinoo@users.noreply.github.com>
2022-12-26 10:49:32 +0000

    7 h/f admin/log_in

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+
+  def after_sign_in_path_for(resource)
+    public_posts_path
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
