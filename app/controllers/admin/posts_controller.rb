3f1ba531fad6f024a3af92f46fffc4a0cca2af4f
yooshiinoo <112680984+yooshiinoo@users.noreply.github.com>
2022-12-27 11:40:41 +0000

    15 admin/post/show

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,11 @@
 class Admin::PostsController < ApplicationController
   def show
+    @post = Post.find(params[:id])
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :text)
+  end
+
 end
