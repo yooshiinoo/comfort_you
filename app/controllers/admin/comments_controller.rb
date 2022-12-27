86e9887e751dff11de6a94e4ce76361932f1f6ad
yooshiinoo <112680984+yooshiinoo@users.noreply.github.com>
2022-12-27 12:08:52 +0000

    16 admin/comment/show

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,17 @@
 class Admin::CommentsController < ApplicationController
   def show
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:id])
   end
 
   def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to admin_post_path(@post.id)
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id, :text)
+  end
+
 end
