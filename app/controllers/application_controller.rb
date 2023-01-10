93af7e3999c8f8927abf74b054fd76b39bfb08b1
yooshiinoo <112680984+yooshiinoo@users.noreply.github.com>
2023-01-10 07:21:54 +0000

    19 public/registrations/new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
       new_admin_session_path
