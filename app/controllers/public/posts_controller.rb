21c69bb4a7bbd5d728770c069ba97138ed553d30
yooshiinoo <112680984+yooshiinoo@users.noreply.github.com>
2023-01-17 08:26:13 +0000

    24 public/post/new.show

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,14 +1,29 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
-  
+
   def create
+    @post = Post.new(post_params)
+    @post.user_id = (current_user.id)
+    if @post.save
+      redirect_to public_post_path(@post.id)
+    else
+      @posts = Post.all
+      render :index
+    end
   end
-  
+
   def index
   end
-  
+
   def show
+    @post = Post.find(params[:id])
   end
-  
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :text)
+  end
+
 end
