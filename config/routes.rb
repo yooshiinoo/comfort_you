4ace7625dabb5882da2f6e5bc81245b96d147476
yooshiinoo <112680984+yooshiinoo@users.noreply.github.com>
2023-07-05 09:51:05 +0000

    45 ja_2

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'public/users/guest_sign_in', to: 'public/users/sessions#guest_sign_in'
   end
 
 
